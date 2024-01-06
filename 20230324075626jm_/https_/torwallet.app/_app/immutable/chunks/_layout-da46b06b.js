f313b99e30f0ad178f4f0ea291f29bf092e3fef0
jakia12 <owasi110@gmail.com>
2024-01-06 15:38:50 +0600

    first commit

---
+++ 20230324075626jm_/https_/torwallet.app/_app/immutable/chunks/_layout-da46b06b.js
@@ -0,0 +1,36 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+const e=!0,r=Object.freeze(Object.defineProperty({__proto__:null,prerender:!0},Symbol.toStringTag,{value:"Module"}));export{r as _,e as p};
+
+
+}
+/*
+     FILE ARCHIVED ON 07:56:26 Mar 24, 2023 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 14:08:49 Jun 12, 2023.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 81.866
+  exclusion.robots: 0.149
+  exclusion.robots.policy: 0.139
+  cdx.remote: 0.059
+  esindex: 0.008
+  LoadShardBlock: 43.735 (3)
+  PetaboxLoader3.datanode: 55.266 (4)
+  load_resource: 118.363
+  PetaboxLoader3.resolve: 88.006
+*/
